588af95607f6523ebe6b1957ba6227fe2cd3bde3
seunghyeon kim <dghjsh@gmail.com>
2026-03-20 14:37:07 +0900

    fix: styling issue

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/xujia_studying/', // 레포지토리 이름을 정확히 입력
+  base: '/xujia_studying/', // 반드시 레포지토리 이름과 일치해야 함
 })
