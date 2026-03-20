de0f7b15c6ff1d5c0e28917f1d9820342c3b0b8d
seunghyeon kim <dghjsh@gmail.com>
2026-03-20 14:30:19 +0900

    fix: 원격 저장소 주소 수정 및 재업로드

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/xujia_studying/', // 레포지토리 이름을 정확히 입력
 })
